31fa7c289fee236cccb8fde49f704552da34f9fb
Tyrion122 <167515538+Tyrion122@users.noreply.github.com>
2026-01-29 13:56:40 +0100

    updated
    added functionality and a limit to the textarea
    added css

--- textarea.js
+++ textarea.js
@@ -1,24 +1,17 @@
-let symbols; //можливо лишнє ще незнаю
-const maxSymbols = 250; //обмеження треба ще застосувати
-
 let quantity = document.getElementById("textarea").value; 
-//console.log(quantity + "/250"); //вже не потрібно
 
 document.getElementById("textarea").addEventListener("input", () =>{
     quantity = document.getElementById("textarea").value;
-    //console.log(quantity.length + "/250"); //вже не потрібно
     document.getElementById("symbolsP").innerText = quantity.length + "/250";
+    if (quantity.length == 250){
+    document.getElementById("textarea").style.color = "red";
+    document.getElementById("textarea").style.borderColor = "red";
+    document.getElementById("symbolsP").style.color = "red";
+    }else{
+        document.getElementById("textarea").style.color = "";
+        document.getElementById("textarea").style.borderColor = "";
+        document.getElementById("symbolsP").style.color = "";
+    };
 }); 
 
-if (quantity.length == 250){
-    document.getElementById("textarea").attributes("readOnly", "true");//обмеження не працює 
-};
-
-
-/*
-
-symbols = 5;
-symbols = document.getElementById("symbolsP").innerText  //незнаю ще як реалізувати
-
-*/
 
